57cdf687df2a1b4d9f84d08e02a2436cff1aaa09
Sven Bohm <bohms@msu.edu>
2014-03-10 18:44:41 -0400

    remove extra fixture files and add test for the entry class

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,6 +1,22 @@
 require 'test_helper'
 
 class EntryTest < ActiveSupport::TestCase
+
+  test 'not valid without note' do
+    assert Entry.new(date: Date.today, author: 'test').valid? == false
+  end
+
+  test "not valid without author" do
+    assert Entry.new(date: Date.today, note: "something").valid? == false
+  end
+
+  test "not valid without date" do
+    assert Entry.new(note: "something", author: "test").valid? == false
+  end
+
+  test 'text search' do 
+    assert Entry.text_search('something').to_sql == "Select * from entries where date @@ 'something' or note @@ 'something"
+  end
   # test "the truth" do
   #   assert true
   # end
