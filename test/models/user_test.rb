ff21a70663b39c69fb04dde4e86d622a6fc12879
Sven Bohm <bohms@msu.edu>
2014-03-13 14:06:10 -0400

    make sure that users have names

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'that it requires a name' do
+    u = User.new
+    u.email = 'test@somewhere.com'
+    u.password = 'passwords'
+    refute u.valid?
+  end
 end
