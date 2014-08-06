a2571f45a2b97ca2fa5e38119a123a8a443206be
Sven Bohm <bohms@msu.edu>
2014-08-05 20:07:32 -0400

    recombined tests into one file

--- test/controllers/entries_controller_test.rb
+++ test/controllers/entries_controller_test.rb
@@ -3,6 +3,20 @@ require 'test_helper'
 class EntriesControllerTest < ActionController::TestCase
   include Devise::TestHelpers
 
+  def setup
+    @user = User.create email: 
+      'test@google.com', 
+      name: 'tester', 
+      password:"a really long password"
+    @entry = Entry.create date: Date.today, 
+      note: "something important", author: @user
+  end
+
+  def teardown
+    @user.delete
+    @entry.delete
+  end
+
   test "should get index" do
     get :index
     assert_response :success
@@ -14,38 +28,41 @@ class EntriesControllerTest < ActionController::TestCase
   end
 
   test "anonymous should not get edit" do
-    get :edit, id: 1
+    get :edit, id: @entry
     assert_redirected_to '/users/sign_in'
   end
 
   test "anonoymous should not get update" do
-    patch :update, id: 1
+    patch :update, id: @entry 
     assert_redirected_to '/users/sign_in'
   end
 
   test "anonymous should not get destroy" do
-    delete :destroy, id: 1
+    delete :destroy, id: @entry 
     assert_redirected_to '/users/sign_in'
   end
 
   test "authenticated should get create" do
-    @controller.stub(:signed_in?).and_return(true)
-    post :create
-    assert_redirected_to '/entries'
+    sign_in @user
+    post :create,  entry: {date: Date.today, note: 'Something to say'}
+    assert_redirected_to entries_path
   end
 
   test "authenticated should get edit" do
-    get :edit, id: 1
+    sign_in @user
+    get :edit, id: @entry
     assert_response :success
   end
 
   test "authenticated should get update" do
-    patch :update, id: 1
-    assert_response :success
+    sign_in @user
+    patch :update, id: @entry, entry: {note: "Something new"}
+    assert_redirected_to entries_path
   end
 
   test "authenticated should get destroy" do
-    delete :destroy, id: 1
-    assert_response :success
+    sign_in @user
+    delete :destroy, id: @entry
+    assert_redirected_to entries_path
   end
 end
