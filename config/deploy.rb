1e3fc056abb742a47c387895499a2669b1bee17e
Sven Bohm <bohms@msu.edu>
2015-01-30 11:03:31 -0500

    added app_secret key to .travis.yml and updated deploy script to link in the uploads directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,6 +28,7 @@ set :linked_files, %w{config/database.yml .env}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{tmp/pids tmp/cache tmp/sockets public/uploads}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
